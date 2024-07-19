643169b8948318bafc262628331a582b8098fcf8
MuttalipOnl <muttaliponl@gmail.com>
2024-07-19 17:58:09 +0300

    Update

--- Python/Regression/3-Polynomial Linear Regression/Polynomial_Linear_Regression.py
+++ Python/Regression/3-Polynomial Linear Regression/Polynomial_Linear_Regression.py
@@ -17,7 +17,7 @@ lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.plot(X, lin_reg.predict(X)  , color = 'blue')
 plt.title("Truth or Bluff (Linear Regression)" )
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
